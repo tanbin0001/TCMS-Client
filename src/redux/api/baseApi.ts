8a7c600b6c60c1ccaf67436964fb351e5f2123b6
Ashrafuj Jaman Tanbin <ashrafuj.jaman.tanbin1@gmail.com>
2024-02-27 18:51:53 +0600

    changed baseapi link

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,7 @@ import { RootState } from "../store";
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5000/api',
+        baseUrl: 'https://tcms-job-task-bloackrank-backend.vercel.app/api',
         prepareHeaders: (headers, { getState }) => {
             const token = (getState() as RootState).auth.token;
 
